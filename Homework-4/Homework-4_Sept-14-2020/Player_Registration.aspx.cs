acff90300543436c966529e0e794f9beda4c4faf
MarkLannen <marklannendev@gmail.com>
2020-09-13 17:42:07 -0600

    Start of Homework 4

---
+++ Homework-4/Homework-4_Sept-14-2020/Player_Registration.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Homework_4_Sept_14_2020
+{
+    public partial class Player_Registration : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string userName = Request.QueryString["txtUsername"];
+            lblPlayerWelcome.Text = "Welcome to Griz Esports" + " " + userName;
+        }
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Player_Registration.aspx?txtUsername=" + txtUsername.Text);
+        }
+    }
+}
