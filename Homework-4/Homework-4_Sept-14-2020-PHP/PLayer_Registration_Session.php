a1f94c6d64a1f8e5bb23ae101c3708a27b89dc19
MarkLannen <marklannendev@gmail.com>
2020-09-19 09:56:08 -0600

    fixed session section

--- Homework-4/Homework-4_Sept-14-2020-PHP/PLayer_Registration_Session.php
+++ Homework-4/Homework-4_Sept-14-2020-PHP/PLayer_Registration_Session.php
@@ -3,7 +3,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 
 <head>
-    <title>Make a cookie in PHP</title>
+    <title>Create a session variable PHP</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
         integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous" />
     <link href="Styles.css" rel="stylesheet" />
@@ -12,27 +12,19 @@
 <body>
 
     <div class="container">
-            <h1 class="mb-5 mt-5 text-center">By loading this page you've created a cookie in PHP</h1>
+            <h1 class="mb-5 mt-5 text-center"Create a session</h1>
             
-    <!-- Set Cookie -->
-    <div class="text-white text-center">
-        <?php
-            $cookie_name = "userName";
-            $cookie_value = "Player's Name";
-            setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/"); // 86400 = 1 day
-        ?>
-
-        <?php
-            if(!isset($_COOKIE[$cookie_name])) {
-            echo "Cookie named '" . $cookie_name . "' is not set!";
-            } else {
-            echo "Cookie '" . $cookie_name . "' is set!<br>";
-            echo "Value is: " . $_COOKIE[$cookie_name];
-            }
-        ?>
-        </div>
-
+    <!-- Create Session Variable -->
+    <div class="container">
+        <form action="session.php" method="POST">
+        <input type="text" name="session" placeholder="Enter 'Si Senor' to set session variable">
+        <button type="submit">Start Session</button>
+        </form>
     </div>
+    
+
+
+
 
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
         integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
